6c8e11891cf35a3c55e6f837c91718c855924260
AbdullahAlsheikh <abdulllah1al1sheikh@gmail>
2016-02-18 20:31:12 -0800

    Updated the mainActivity

--- app/src/main/java/com/example/helloworldyelp/MainActivity.java
+++ app/src/main/java/com/example/helloworldyelp/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.TextView;
+import android.text.Html;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -18,6 +19,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Random;
+
 import org.scribe.model.OAuthRequest;
 import org.scribe.model.Response;
 import org.scribe.model.Token;
@@ -34,43 +37,60 @@ public class MainActivity extends AppCompatActivity {
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
+    Random ran = new Random();
     String consumerKey = "dudmo3ssHxvpUP_i_Lw60A";
     String consumerSecret = "fOhwH5mUo_CyzX2D2vcDUc8FNw8";
-    String token = "pppXtWQAG-DcMV5HbeS7Cax81Dua98ea";
-    String tokenSecret = "NqEsSpS6aOJpWQCDT_ox_-HYZLU";
+    String token = "yPhkb0u9cRxGE8ikWRkH3ceMCCpKYpQA";
+    String tokenSecret = "-WoZd39mwu4X9iVDXo5bxDNOBBU";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
 
 
-        final TextView tv = (TextView) findViewById(R.id.textView2);
-        Button generator = (Button) findViewById(R.id.button);
-        generator.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+                final TextView tv = (TextView) findViewById(R.id.textView2);
+                Button generator = (Button) findViewById(R.id.button);
+                String htmlexample = "<body><h2>The Result<br></h2>";
+                tv.setText(Html.fromHtml(htmlexample, null, null));
+                generator.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        //System.out.println("Within the Thread");
                 Thread t = new Thread(){
                     public void run(){
                         try {
+
+                            int RandResturant =  ran.nextInt(10  - 2) + 1;
+                            Long tsLong = System.currentTimeMillis()/1000;
+                            String ts = tsLong.toString();
+                            System.out.println(ts);
                             Yelp yelp = new Yelp(consumerKey, consumerSecret, token, tokenSecret);
                             String response = yelp.search("burritos", 30.361471, -87.164326);
+                            System.out.println(response);
+
                             //System.out.println("Result:" + response);
+//                            YelpParser yp = new YelpParser();
+//                            yp.setResponse(response);
+//                            yp.parseBusiness();
+//                            String activity = yp.getBusinessName(RandResturant);
+//                            System.out.print(activity);
+//                            String htmlexample = "<body><h2>The Result<br></h2><p>"+activity+"<p>";
+//
+//                            tv.setText(Html.fromHtml(htmlexample, null, null));
+                              } catch (Exception e) {
 
-                            YelpParser yp = new YelpParser();
-                            yp.setResponse(response);
-                            yp.parseBusiness();
-                            String activity = yp.getBusinessName(0);
-                            //System.out.print(activity);
-                            tv.setText(activity);
-                        } catch (Exception e) {
+                                  System.out.println("\n\n\nError:"+e.getMessage());
+                                }
 
-                            System.out.println("\n\n\nError:"+e.getMessage());
-                        }
-                    }
-                };
+                             }
+                         };
+
+                       t.start();
 
 
-                t.start();
+
+
+
 
 
             }
